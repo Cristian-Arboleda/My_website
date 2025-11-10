7521ef2010f58a9b568a689490ea08d73a74b36d
Cristian Arboleda <cristianarboledagarzon@gmail.com>
2025-11-10 14:50:33 -0500

    projects en desarrollo

--- pages/projects.py
+++ pages/projects.py
@@ -2,6 +2,7 @@ from dash import register_page, html, dcc, callback, Input, Output, State, no_up
 
 register_page(__name__)
 
+url_img = 'assets/images/project/'
 project_data = [
     {
         'title': 'twitch_notificacion_bot', 
@@ -40,39 +41,72 @@ project_data = [
     }
 ]
 
-def project(index=0):
+def create_project(index=0):
     return html.A(
         href=project_data[index]['url'],
-        id='project_data_element',
+        className='carousel_project_element',
         target='_blank',
         children=[
-            html.P(project_data[index]['title'].replace('_', ' ').title()),
+            html.P(
+                project_data[index]['title'].replace('_', ' ').title(),
+                className='carousel_title',
+            ),
             html.Img(
-                src='assets/images/project/' + project_data[index]['img'] + '.png',
-                className='project_img',
+                src=url_img + project_data[index]['img'] + '.png',
+                className='carousel_project_img',
             ),
             html.P(project_data[index]['description'])
         ],
     )
 
+project_grid = html.Div(
+    id='project_grid_container',
+    children=[
+        html.A(
+            className='project_grid_element',
+            children=[
+                html.P(
+                    project['title'],
+                    style={'grid-row': '1'}
+                ),
+                html.Img(
+                    src = url_img + project['img'] + '.png',
+                    className='project_grid_img',
+                    style={'grid-row': '2'}
+                ),
+                html.P(
+                    project['description'],
+                    style={'grid-row': '3'}
+                ),
+                html.P(
+                    'tools',
+                    style={'grid-row': '4'}
+                )
+            ]
+        )
+        for project in project_data
+    ]
+)
+
 layout = html.Div(
     id='project_container',
     children=[
         html.Div(
-            id='carousel_container',
+            className='carousel_project_container',
             children=[
                 html.Button('⟨', id='prev_btn', className='btn_carousel'),
-                html.Div(id='project_data_container', children=project()),
+                html.Div(id='carousel_project_container', children=create_project()),
                 html.Button('⟩', id='next_btn', className='btn_carousel'),
                 dcc.Store(id='current_index', data=0)
             ]
         ),
+        project_grid,
     ]
 )
 
 @callback(
     Output('current_index', 'data'),
-    Output('project_data_container', 'children'),
+    Output('carousel_project_container', 'children'),
     Input('next_btn', 'n_clicks'),
     Input('prev_btn', 'n_clicks'),
     Input('current_index', 'data'),
@@ -81,18 +115,18 @@ layout = html.Div(
 def update_carousel(next_btn, prev_btn, current_index):
     
     trigger = ctx.triggered_id
-    total_projects = len(project_data) - 1
+    total_projects = len(project_data)
     print('total_projects', total_projects)
     
     if trigger == 'next_btn':
         current_index += 1
-        if current_index > total_projects:
+        if current_index > (total_projects - 1):
             current_index = 0
     
     elif trigger == 'prev_btn':
         current_index -= 1
-        if current_index < -(total_projects + 1):
-            current_index = 1
+        if current_index < -total_projects:
+            current_index = -1
     
     print(current_index)
-    return current_index, project(current_index)
+    return current_index, create_project(current_index)
